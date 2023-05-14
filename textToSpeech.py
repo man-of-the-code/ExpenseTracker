e07adbb83c32e854d1d17c2461283e5b8e32827d
man-of-the-code <rnmahato1996@gmail.com>
2023-05-14 16:11:34 +0530

    python code to convert text to speech using gtts library

---
+++ textToSpeech.py
@@ -0,0 +1,38 @@
+from gtts import gTTS
+
+# language = "en" 
+language = "hi"
+# text = "Hello and welcome to today's video. In this episode, we'll be discussing four ways to access GPT-4 for free without Chat GPT Plus."
+text = "जीपीएस (GPS) और वीजुअल पोजीशनिंग सिस्टम (VPS) दोनों ही स्थान निर्धारण सिस्टम हैं लेकिन ये दोनों अलग-अलग कार्यों के लिए बनाए गए हैं।"
+speech = gTTS(text=text, lang=language, slow=False, tld="com.au") 
+speech.save("textToSpeech.mp3")
+
+
+
+
+# Import the required module for text
+# to speech conversion
+# from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+# import os
+
+# The text that you want to convert to audio
+# mytext = 'Welcome to geeksforgeeks!'
+
+# Language in which you want to convert
+# language = 'en'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+# myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+# myobj.save("welcome.mp3")
+
+# Playing the converted file
+# os.system("mpg321 welcome.mp3")
